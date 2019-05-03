fc14ea1af8b9476aab3636c4dc4ecaffbd9d856b
Ammon Cisneros <ammon.15.2015.ci@gmail.com>
2019-05-03 13:05:04 -0700

    Initial-commit 0.1

---
+++ src/bootstrap.js
@@ -0,0 +1,7 @@
+    // import moment from 'moment';
+
+    // const twoWeeksFromNow = moment().add(14, 'days');
+    // // const birthday = moment('2018-02-05', 'YYYY-MM-DD');
+    // // console.log(birthday.fromNow())
+    // console.log(twoWeeksFromNow)
+    console.log("TESssssssssssssssssssT");
